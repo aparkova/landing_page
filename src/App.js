b348598114b96529e48061df1e44e17664c3b43d
Adriana <adriana.parkova@rwth-aachen.de>
2022-05-16 12:58:00 +0200

    myedulife landing page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './myedulife_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         MyEduLife Project
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
